7a6a97971f4ac3b0f524b6863617a601f3d5e8d5
Juanma <164524389+JuanManuelRama@users.noreply.github.com>
2024-04-08 20:00:37 -0300

    Añadí TP°|

---
+++ TP°1/SueldosDocentes.hs
@@ -0,0 +1,42 @@
+type Cargo=String
+type Horas=Int
+type Antiguedad=Int
+type Sueldo=Float
+type Porcentaje=Float
+
+
+calcSueldo :: Horas->Cargo->Antiguedad->Sueldo
+calcSueldo horas cargo antiguedad = basico cargo * bonAntiguedad antiguedad * bonHoras horas
+
+
+basico :: Cargo->Float
+basico "titular"=149000
+basico "adjunto"=116000
+basico "ayudante"=66000
+basico x = error "No es un cargo"
+
+
+bonAntiguedad :: Antiguedad->Float
+bonAntiguedad antiguedad
+    |antiguedad <3  = porcentaje 10
+    |antiguedad <5  = porcentaje 20
+    |antiguedad <10 = porcentaje 30
+    |antiguedad <24 = porcentaje 50
+    |antiguedad >24 = porcentaje 120
+
+porcentaje :: Float->Porcentaje
+porcentaje x = (x/100)+1
+
+
+bonHoras :: Horas->Float    
+bonHoras horas 
+    |horas<5 || horas>50 = error "No se puede haber trabajado esa cantidad de horas"
+    |otherwise = fromIntegral (redondearUnidad horas) / 10
+
+redondearUnidad :: Int->Int
+redondearUnidad x 
+    |truncDecena x >=5 = x+(10-truncDecena x)    
+    |otherwise = x-truncDecena x
+
+truncDecena :: Int->Int
+truncDecena x =mod x 10

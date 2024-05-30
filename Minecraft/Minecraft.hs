5146f35b06915c478294341587e1e5d2335c110e
Juanma <jrama@frba.utn.edu.ar>
2024-05-30 20:31:33 -0300

    Añado resolución a Minecraft

--- Minecraft/Minecraft.hs
+++ Minecraft/Minecraft.hs
@@ -1,5 +1,6 @@
 type Cambio = Personaje->Personaje
 type Material = String
+type Herramienta = [Material]->Material
 
 data Personaje = UnPersonaje {
    nombre :: String,
@@ -12,44 +13,50 @@ data Receta = UnaReceta{
     tiempo :: Int,
     requisitos :: [Material]
 }
+data Bioma = UnBioma{
+    loot :: [Material],
+    requisito :: Material
+}
 
 rFogata :: Receta
 rFogata = UnaReceta "Fogata" 10 ["Madera", "Fosforo"]
 rPolloAsado :: Receta
 rPolloAsado = UnaReceta "Pollo Asado"  300 ["Fogata", "Pollo"]
 
+hacha :: Herramienta
+hacha = last
+espada :: Herramienta
+espada = head
+pico ::Int->Herramienta
+pico n = (!! n)
+artico :: Bioma
+artico = UnBioma ["Hielo", "Iglu", "Lobo"] "Sueter"
+
+
+
+granCambio :: Personaje->[Cambio]->Personaje
+granCambio = foldr ($)
 
 craftear :: Receta->Cambio
 craftear receta personaje
-    |puedeCrafter personaje receta  = (quitarMateriales receta.crafteroExito receta.aniadirObjeto receta) personaje
-    |otherwise = crafteoFracaso personaje
+    |puedeCraftear personaje receta  = granCambio personaje [(aniadirObjeto.nombreO) receta, crafteroExito receta, quitarMateriales receta]
+    |otherwise = cambiarPuntos (-100) personaje
 
 
 
 
-aniadirObjeto:: Receta->Cambio
-aniadirObjeto receta personaje = personaje{inventario = nombreO receta : inventario personaje}
+aniadirObjeto:: Material->Cambio
+aniadirObjeto objeto personaje = personaje{inventario = objeto : inventario personaje}
 
 quitarMateriales::Receta->Cambio
-quitarMateriales receta personaje = personaje{inventario = craftear1 (inventario personaje) (requisitos receta)}
-
-
-craftear1 :: Ord a => [a] -> [a] -> [a]
-craftear1 [] _ = []
-craftear1 lista [] = lista
-craftear1 x  (y:ys)
-    |x==y = craftear1 xs ys
-    |otherwise = craftear1 (xs++[x]) [y]++ys
+quitarMateriales receta personaje = personaje{inventario = foldl quitarMaterial (inventario personaje) (requisitos receta)}
 
 
 
-
-{-quitarMateriales::[Materiales]->[Materiales]->[Materiales]
-quitarMateriales [] [_] = []
-quitarMateriales x [] = x
-quitarMateriales (x:xs) (y:ys)
-    |x==y = quitarMateriales xs ys
-    |otherwise quitarMateriales-}
+quitarMaterial :: [Material]->Material->[Material]
+quitarMaterial (primero:resto) requisito
+    |primero==requisito = resto
+    |otherwise = quitarMaterial (resto++[primero]) requisito
 
 cambiarPuntos :: Int->Cambio
 cambiarPuntos aumento personaje = personaje{puntaje = puntaje personaje + aumento}
@@ -57,15 +64,33 @@ cambiarPuntos aumento personaje = personaje{puntaje = puntaje personaje + aument
 crafteroExito :: Receta->Cambio
 crafteroExito receta = cambiarPuntos (10*tiempo receta)
 
-crafteoFracaso :: Cambio
-crafteoFracaso = cambiarPuntos (-100)
 
-
-
-puedeCrafter::Personaje->Receta->Bool
-puedeCrafter personaje receta = all (tieneMaterial (inventario personaje)) (requisitos receta)
+puedeCraftear::Personaje->Receta->Bool
+puedeCraftear personaje receta = all (tieneMaterial (inventario personaje)) (requisitos receta)
 
 tieneMaterial :: [Material]->Material->Bool
 tieneMaterial materiales material = material `elem` materiales
 
 
+quePuedeCraftear :: Personaje->[Receta]->[Receta]
+quePuedeCraftear personaje = filter  (puedeYDuplica personaje)
+
+puedeYDuplica :: Personaje -> Receta -> Bool
+puedeYDuplica personaje receta = puedeCraftear personaje receta && duplicaPuntaje personaje receta
+
+duplicaPuntaje :: Personaje->Receta->Bool
+duplicaPuntaje personaje receta = (puntaje.crafteroExito receta) personaje > 2* puntaje personaje
+
+multiCrafteo :: [Receta]->Cambio
+multiCrafteo recetas personaje = foldr craftear personaje recetas
+
+mejorOInverso :: Personaje->[Receta]->Bool
+mejorOInverso personaje recetas = (puntaje.multiCrafteo recetas) personaje > (puntaje.multiCrafteo (reverse recetas)) personaje
+
+minar:: Herramienta->Bioma->Cambio
+minar herramienta bioma personaje
+     |puedeMinar personaje bioma = granCambio personaje [cambiarPuntos 50, (aniadirObjeto.herramienta.loot) bioma]
+     |otherwise = personaje
+
+puedeMinar :: Personaje -> Bioma -> Bool
+puedeMinar personaje = (tieneMaterial.inventario) personaje.requisito

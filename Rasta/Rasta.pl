a392dcd777c6568c9e4a73d6477e6b1a50f11ab7
Juanma <jrama@frba.utn.edu.ar>
2024-06-15 16:15:53 -0300

    prolog

---
+++ Rasta/Rasta.pl
@@ -0,0 +1,20 @@
+enAula(rasta).
+enAula(polito).
+enAula(santi).
+
+ayuda(rasta, Persona):-
+    enAula(Persona),
+    quiere(rasta, Persona),
+    not(compararSuerte(rasta, Persona)).
+
+quiere(santi, Persona):- not(quiere(rasta, Persona)).
+
+quiere(rasta, Persona):-
+    enAula(Persona),
+    not(Persona==santi).
+
+quiere(polito, Persona) :-quiere(rasta, Persona).
+
+compararSuerte(rasta, Persona):-quiere(polito, Persona).
+
+
